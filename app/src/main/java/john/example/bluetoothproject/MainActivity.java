55be6c218cbe50b6586acbc0110fe96008597508
waaade <johnlarson562@gmail.com>
2019-03-13 09:52:58 -0600

    Add button to launch chat, with corresponding function in MainActivity

--- app/src/main/java/john/example/bluetoothproject/MainActivity.java
+++ app/src/main/java/john/example/bluetoothproject/MainActivity.java
@@ -1,7 +1,9 @@
 package john.example.bluetoothproject;
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void launchChat(View view) {
+        Intent intent = new Intent(this, BluetoothChat.class);
+        startActivity(intent);
+    }
+
 }
